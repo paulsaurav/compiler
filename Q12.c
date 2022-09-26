0a4d1495c84e9347cfae7de28754c64acdd7be8b
SAURAV PAUL <72635245+paulsaurav@users.noreply.github.com>
2022-09-26 13:33:33 -0700

    Add files via upload

--- Q12.c
+++ Q12.c
@@ -1,31 +1,29 @@
-               #include<stdio.h>
-               #include<stdlib.h>
-               #include<string.h>
-               int main()
-               {
-                    char string[50];
-                    int count=0,i;
-                    printf("Enter the String: ");
-                    gets(string);
-                   //int len=strlen(string);
-                   //printf("length:%d",len);
-                    if((string[0]>='a'&&string[0]<='z') || (string[0]>='A'&&string[0]<='Z') || (string[0]=='_') || (string[0]=='$'))
-                    {
-                                for(i=1;i<=strlen(string);i++)
-                                {
-                                            if((string[i]>='a'&& string[i]<='z') || (string[i]>='A' && string[i]<='Z') || (string[i]>='0'&& string[i]<='9') || (string[i]=='_'))
-                                            {
-                                                        count++;
-                                            }
-                                }
-                    }
-                    if(count==(strlen(string)-1))
-                   {
-                                printf("Input string is a valid identifier");
-                   }
-                   else
-                   {
-                                printf("Input string is not a valid identifier");
-                    }
-                    return 0;
-               }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main()
+{
+char a[10];
+int flag, i=1;
+
+printf("\n Enter an identifier:");
+gets(a);
+if(isalpha(a[0]))
+flag=1;
+else
+printf("\n Not a valid identifier");
+while(a[i]!='\0')
+{
+if(!isdigit(a[i])&&!isalpha(a[i]))
+{
+flag=0;
+break;
+}
+i++;
+}
+if(flag==1)
+printf("\n Valid identifier");
+getch();
+}
+
